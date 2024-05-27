e414fabb5d5984a83f5f8ed64acb1b6982866d2a
Anjaneesh-valluri <anjaneeshv@gmail.com>
2024-05-27 17:26:24 +0530

    made some changes

--- KnowIt/urls.py
+++ KnowIt/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+
+app_name="KnowIt"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("second/", views.second, name="second")
+    path("physicists/", views.physicists, name="physicists"),
+    path("form/",views.form,name="form"),
+    path("form/display/",views.display,name="display")
 ]
--- KnowIt/views.py
+++ KnowIt/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+form1 = []
 def index(request):
     return render(request,"KnowIt/index.html")
-def second(request):
-    return render(request,"KnowIt/second.html")
+def physicists(request):
+    return render(request,"KnowIt/physicists.html")
+def form(request):
+    if request.method=="Post":
+        form1= request.post
+        return render(request,"KnowIt/display.html",{"form":form1})
+    return render(request,"KnowIt/form.html")
+
+def display(request):
+    email = request.GET['inlineFormInputGroupUsername']
+    #value = request.GET['inlineFormCheck']
+    form = [email]
+
+
+    return render(request,"KnowIt/display.html",{"form": form})
